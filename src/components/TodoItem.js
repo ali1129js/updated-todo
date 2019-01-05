b208b758e0031b5629a281f1d82d2d69b0171ed4
ali1129js <ali1129js@gmail.com>
2019-01-05 10:02:14 +0100

    added PropTypes

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,10 +2,14 @@
  * @Author: Ali
  * @Date:   2019-01-05T09:25:48+01:00
  * @Last modified by:   Ali
- * @Last modified time: 2019-01-05T09:29:38+01:00
+ * @Last modified time: 2019-01-05T10:01:31+01:00
  */
 import React from "react";
+import PropTypes from "prop-types";
 
 const TodoItem = props => <div>{props.todo.title}</div>;
 
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired
+};
 export default TodoItem;
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -2,9 +2,10 @@
  * @Author: Ali
  * @Date:   2019-01-05T09:16:28+01:00
  * @Last modified by:   Ali
- * @Last modified time: 2019-01-05T09:30:14+01:00
+ * @Last modified time: 2019-01-05T09:59:09+01:00
  */
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import TodoItem from "./TodoItem";
 
 class Todos extends Component {
@@ -12,4 +13,7 @@ class Todos extends Component {
     return this.props.todos.map(todo => <TodoItem key={todo.id} todo={todo} />);
   }
 }
+Todos.propTypes = {
+  todos: PropTypes.array.isRequired
+};
 export default Todos;
